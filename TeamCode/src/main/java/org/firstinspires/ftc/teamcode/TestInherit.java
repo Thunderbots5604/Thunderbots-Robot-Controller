0652f3c18f926c2f20cc7e5f10ed3e66a956b385
knightsean00 <knightsean00@gmail.com>
2018-12-13 16:37:43 -0800

    :)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestInherit.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestInherit.java
@@ -8,10 +8,17 @@ import com.qualcomm.robotcore.hardware.DistanceSensor;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
+import org.firstinspires.ftc.robotcore.external.ClassFactory;
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer.CameraDirection;
+import org.firstinspires.ftc.robotcore.external.tfod.Recognition;
+import org.firstinspires.ftc.robotcore.external.tfod.TFObjectDetector;
 
-@Autonomous(name="Detach Only", group="Autonomous Competition")
-public class AutoDetach extends LinearOpMode {
+import java.util.List;
+
+@Autonomous(name="Please Inherit", group="Autonomous Competition")
+public class TestInherit extends AutoDetach {
 
     // Declare OpMode members.
     private ElapsedTime runtime = new ElapsedTime();
@@ -29,6 +36,38 @@ public class AutoDetach extends LinearOpMode {
     private DistanceSensor distance = null;
     private final double INCHES_PER_TICK = .0223147377;
     private final double DEGREES_PER_TICK = .17106201;
+
+    private int location = -1;
+    private int objects = 0;
+    private static final String TFOD_MODEL_ASSET = "RoverRuckus.tflite";
+    private static final String LABEL_GOLD_MINERAL = "Gold Mineral";
+    private static final String LABEL_SILVER_MINERAL = "Silver Mineral";
+
+    /*
+     * IMPORTANT: You need to obtain your own license key to use Vuforia. The string below with which
+     * 'parameters.vuforiaLicenseKey' is initialized is for illustration only, and will not function.
+     * A Vuforia 'Development' license key, can be obtained free of charge from the Vuforia developer
+     * web site at https://developer.vuforia.com/license-manager.
+     *
+     * Vuforia license keys are always 380 characters long, and look as if they contain mostly
+     * random data. As an example, here is a example of a fragment of a valid key:
+     *      ... yIgIzTqZ4mWjk9wd3cZO9T1axEqzuhxoGlfOOI2dRzKS4T0hQ8kT ...
+     * Once you've obtained a license key, copy the string from the Vuforia web site
+     * and paste it in to your code on the next line, between the double quotes.
+     */
+    private static final String VUFORIA_KEY = "AfDTG3b/////AAABmQnAWkqPDkVztdcIurPa8w5lO6BWlTpltO5r1m4s9oA9w3cfFdIBqJ4rjZB0We4YHcRMdc5LkWwOvJk+xcDr2VFpLP8m6zEqLWlrVNQvBnNxmGO8BeZ+xRQeQ34AgPhrqQQqeheWJbfoOn+lekIz2ZMm9f+j+1ng/X0vKDHyFGfxbtXbJuUx4Qh6E3t0esH0b3VQtbuJiOOTpWi9xFAqBsHWp+DQbwub+a6HZV5q42OabnOAyr0GZ7u1vJZs+I/Vlnf7qEMLD4RTIYA5OmMyzOdl5aikZqDSgG223ETSwcbwd3QFKewYE3oXXxkpI0vmsxCiaqBJ1oL9e6n0RXbC8Zdvn2VYwh6oSemcpp+fSjGa";
+
+    /**
+     * {@link #vuforia} is the variable we will use to store our instance of the Vuforia
+     * localization engine.
+     */
+    private VuforiaLocalizer vuforia;
+
+    /**
+     * {@link #tfod} is the variable we will use to store our instance of the Tensor Flow Object
+     * Detection engine.
+     */
+    private TFObjectDetector tfod;
 
     @Override
     public void runOpMode() {
@@ -52,46 +91,14 @@ public class AutoDetach extends LinearOpMode {
         distance = hardwareMap.get(DistanceSensor.class, "distance");
 
         waitForStart();
-
-        while (distance.getDistance(DistanceUnit.MM) > 100) {
-            telemetry.addLine("Phase: Lowering Part 1");
-            telemetry.addData("Distance", distance.getDistance(DistanceUnit.MM));
-            telemetry.update();
-            crane1.setPower(.75);
-            crane2.setPower(-.75);
-        }
-        runtime.reset();
-        while (distance.getDistance(DistanceUnit.MM) > 46) {
-            telemetry.addLine("Phase: Lowering Part 2");
-            telemetry.addData("Distance", distance.getDistance(DistanceUnit.MM));
-            telemetry.update();
-            crane1.setPower(.55);
-            crane2.setPower(-.55);
-        }
-        sleep(250);
-        crane1.setPower(0);
-        crane2.setPower(0);
-        sleep(100);
-        runtime.reset();
-        while(runtime.milliseconds() < 75) {
-            crane1.setPower(-.55);
-            crane2.setPower(.55);
-        }
-        crane1.setPower(0);
-        crane2.setPower(0);
-
-        sleep(500);
-
-        if(distance.getDistance(DistanceUnit.MM) < 80) {
-            runTo(1.5, .25);
-            sleep(100);
-            turnRight(45, .55);
-            runTo(10, .25);
-            sleep(100);
-            turnLeft(170, .55);
-            runTo(-6, .25);
-            runTo(-3, .25);
-        }
+        //Stuff to display for Telemetry
+        turnRight(45, .35);
+        sleep(2500);
+        turnLeft(90,.45);
+        sleep(2500);
+        turnLeft(180, .55);
+        sleep(2500);
+        turnRight(360, .65);
     }
 
     private void runTo(double inches, double power) {
@@ -251,5 +258,32 @@ public class AutoDetach extends LinearOpMode {
         rightMotorFront.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         rightMotorBack.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
     }
-}
+    /**
+     * Initialize the Vuforia localization engine.
+     */
+    private void initVuforia() {
+        /*
+         * Configure Vuforia by creating a Parameter object, and passing it to the Vuforia engine.
+         */
+        VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters();
 
+        parameters.vuforiaLicenseKey = VUFORIA_KEY;
+        parameters.cameraDirection = CameraDirection.BACK;
+
+        //  Instantiate the Vuforia engine
+        vuforia = ClassFactory.getInstance().createVuforia(parameters);
+
+        // Loading trackables is not necessary for the Tensor Flow Object Detection engine.
+    }
+
+    /**
+     * Initialize the Tensor Flow Object Detection engine.
+     */
+    private void initTfod() {
+        int tfodMonitorViewId = hardwareMap.appContext.getResources().getIdentifier(
+                "tfodMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        TFObjectDetector.Parameters tfodParameters = new TFObjectDetector.Parameters(tfodMonitorViewId);
+        tfod = ClassFactory.getInstance().createTFObjectDetector(tfodParameters, vuforia);
+        tfod.loadModelFromAsset(TFOD_MODEL_ASSET, LABEL_GOLD_MINERAL, LABEL_SILVER_MINERAL);
+    }
+}
