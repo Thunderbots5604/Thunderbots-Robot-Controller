b61649752e107e2827657e3cc3e80603fa1c88c8
klhftco <klhftco@gmail.com>
2019-02-17 22:54:39 -0800

    Most of Depot to Crater

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousDepotToCrater.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousDepotToCrater.java
@@ -19,53 +19,56 @@ public class AutonomousDepotToCrater extends GodfatherOfAllAutonomous {
 
         detach();
 
-        location = tfodDetection(6);
+        location = tfodDetection(4);
         if (location == 2) {
             turnRight(25,allPower);
             runTo(31, allPower);
             sleep(500);
-            turnLeft(60,allPower);
+            turnLeft(95,allPower);
             runTo(20,allPower);
         }
         if (location == 1) {
-            turnLeft(12, allPower);
-            runTo(21,allPower);
-            sleep(500);
+            //Hit Marker
+            turnLeft(18, allPower);
+            runTo(18,allPower);
+            sleep(250);
         }
         if (location == 0) {
-            turnLeft(50, allPower);
-            runTo(32,allPower);
+            turnLeft(55, allPower);
+            runTo(35,allPower);
             sleep(250);
-            turnRight(110,allPower);
-            runTo(20,allPower);
+            turnRight(150,allPower);
+            runTo(22,allPower);
         }
         dropMarker();
         if (location == 2) {
             runTo(-14,allPower);
             turnRight(60,allPower);
             runTo(-18,allPower);
-            turnLeft(10,allPower);
-            turnLeft(60,allPower);
+            turnLeft(66,allPower);
             runTo(7,allPower);
-            turnLeft(30,allPower);
-            runTo(10,allPower);
+            turnLeft(40,allPower);
+            runTo(20,allPower);
             turnLeft(20,allPower);
-            runTo(60,.9);
+            runTo(50,.9);
         }
         if (location == 1) {
-            runTo(-28,allPower);
+            //Go Back
+            turnLeft(5,allPower);
+            runTo(-16,allPower);
+            //Adjust
             turnRight(8,allPower);
-            turnLeft(60,allPower);
-            runTo(7,allPower);
-            turnLeft(30,allPower);
-            runTo(10,allPower);
-            turnLeft(20,allPower);
-            runTo(60,.9);
+            runTo(2.5,allPower);
+            //Go to Crater
+            turnLeft(66.6,allPower);
+            runTo(50.5,allPower);
+            turnLeft(42.0,allPower);
+            runTo(32,allPower);
         }
         if (location == 0) {
             runTo(-5, allPower);
-            turnRight(15, allPower);
-            runTo(-85, .8);
+            turnRight(20, allPower);
+            runTo(-80, .8);
         }
     }
 }
