b8c5d2dfd48aa97002cdbcaeb7ff7b8c08d0a6ee
5jack05 <masterjackkc@gmail.com>
2019-10-29 17:16:24 -0700

    Update TeleOpAlpha.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpAlpha.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpAlpha.java
@@ -21,12 +21,12 @@ public class TeleOpAlpha extends LinearOpMode {
     private DcMotor leftMotorBack = null;
     private DcMotor rightMotorFront = null;
     private DcMotor rightMotorBack = null;
-    private Servo clawServo = null;
-    private Servo armServo = null;
+    private CRServo clawServo = null;
+    private CRServo armServo = null;
 
     private boolean reversed = false;
     private boolean halfSpeed = false;
-    private double multiplier = 1;
+    private double multiplier = -1;
 
     @Override
     public void runOpMode() {
@@ -36,17 +36,12 @@ public class TeleOpAlpha extends LinearOpMode {
         leftMotorBack = hardwareMap.get(DcMotor.class, "left_motor_back");
         rightMotorFront = hardwareMap.get(DcMotor.class, "right_motor_front");
         rightMotorBack = hardwareMap.get(DcMotor.class, "right_motor_back");
-        clawServo = hardwareMap.get(Servo.class, "claw_servo");
-        armServo = hardwareMap.get(Servo.class, "arm_servo");
+        clawServo = hardwareMap.get(CRServo.class, "claw_servo");
+        armServo = hardwareMap.get(CRServo.class, "arm_servo");
 
         leftMotorFront.setDirection(DcMotorSimple.Direction.REVERSE);
         leftMotorBack.setDirection(DcMotorSimple.Direction.REVERSE);
 
-        armServo.setPosition(0);
-        clawServo.setPosition(0);
-
-        armServo.getDirection();
-        clawServo.getDirection();
         telemetry.addData("Status", "Initialized");
         telemetry.update();
 
@@ -57,12 +52,11 @@ public class TeleOpAlpha extends LinearOpMode {
         while (opModeIsActive()) {
 
             telemetry.addData("Left Front Power: ", leftMotorFront.getPower());
-            telemetry.addData("Left Back Power: ", leftMotorBack.getPower());
             telemetry.addData("Right Front Power: ", rightMotorFront.getPower());
-            telemetry.addData("Right Back Power: ", rightMotorBack.getPower());
             telemetry.addData("Reversed: ", reversed);
-            telemetry.addData("Claw Servo", clawServo.getPosition());
-            telemetry.addData("Arm Servo", armServo.getPosition());
+            telemetry.addData("Half Speed" , halfSpeed);
+            telemetry.addData("Claw Servo", clawServo.getPower());
+            telemetry.addData("Arm Servo", armServo.getPower());
             telemetry.update();
 
             if(gamepad1.b && cooldown.seconds() > .5) {
@@ -101,31 +95,31 @@ public class TeleOpAlpha extends LinearOpMode {
             //Swerve Turn
             //Left Turn
             else if (gamepad1.left_stick_y != 0 && gamepad1.right_stick_x < 0 && !reversed) {
-                leftMotorFront.setPower(multiplier * (gamepad1.right_stick_x / 4 + gamepad1.left_stick_y / 4));
-                leftMotorBack.setPower(multiplier * (gamepad1.right_stick_x / 4 + gamepad1.left_stick_y / 4));
-                rightMotorFront.setPower(multiplier * (gamepad1.right_stick_x / 2 + gamepad1.left_stick_y / 2));
-                rightMotorBack.setPower(multiplier * (gamepad1.right_stick_x / 2 + gamepad1.left_stick_y / 2));
+                leftMotorFront.setPower(0);
+                leftMotorBack.setPower(0);
+                rightMotorFront.setPower(multiplier * (gamepad1.left_stick_y));
+                rightMotorBack.setPower(multiplier * (gamepad1.left_stick_y));
             }
             //Right Turn
             else if (gamepad1.left_stick_y != 0 && gamepad1.right_stick_x > 0 && !reversed) {
-                leftMotorFront.setPower(multiplier * (gamepad1.right_stick_x / 2 + gamepad1.left_stick_y / 2));
-                leftMotorBack.setPower(multiplier * (gamepad1.right_stick_x / 2 + gamepad1.left_stick_y / 2));
-                rightMotorFront.setPower(multiplier * (gamepad1.right_stick_x / 4 + gamepad1.left_stick_y / 4));
-                rightMotorBack.setPower(multiplier * (gamepad1.right_stick_x / 4 + gamepad1.left_stick_y / 4));
+                leftMotorFront.setPower(multiplier * (gamepad1.left_stick_y));
+                leftMotorBack.setPower(multiplier * (gamepad1.left_stick_y));
+                rightMotorFront.setPower(0);
+                rightMotorBack.setPower(0);
             }
             //Reverse Left Turn
-            else if (gamepad1.left_stick_y != 0 && gamepad1.right_stick_x > 0 && reversed) {
-                leftMotorFront.setPower(-multiplier * (gamepad1.right_stick_x / 4 + gamepad1.left_stick_y / 4));
-                leftMotorBack.setPower(-multiplier * (gamepad1.right_stick_x / 4 + gamepad1.left_stick_y / 4));
-                rightMotorFront.setPower(-multiplier * (gamepad1.right_stick_x / 2 + gamepad1.left_stick_y / 2));
-                rightMotorBack.setPower(-multiplier * (gamepad1.right_stick_x / 2 + gamepad1.left_stick_y / 2));
+            else if (gamepad1.left_stick_y != 0 && gamepad1.right_stick_x < 0 && reversed) {
+                leftMotorFront.setPower(multiplier * (gamepad1.left_stick_y));
+                leftMotorBack.setPower(multiplier * (gamepad1.left_stick_y));
+                rightMotorFront.setPower(0);
+                rightMotorBack.setPower(0);
             }
             //Reverse Right Turn
-            else if (gamepad1.left_stick_y != 0 && gamepad1.right_stick_x < 0 && reversed) {
-                leftMotorFront.setPower(-multiplier * (gamepad1.right_stick_x / 2 + gamepad1.left_stick_y / 2));
-                leftMotorBack.setPower(-multiplier * (gamepad1.right_stick_x / 2 + gamepad1.left_stick_y / 2));
-                rightMotorFront.setPower(-multiplier * (gamepad1.right_stick_x / 4 + gamepad1.left_stick_y / 4));
-                rightMotorBack.setPower(-multiplier * (gamepad1.right_stick_x / 4 + gamepad1.left_stick_y / 4));
+            else if (gamepad1.left_stick_y != 0 && gamepad1.right_stick_x > 0 && reversed) {
+                leftMotorFront.setPower(0);
+                leftMotorBack.setPower(0);
+                rightMotorFront.setPower(multiplier * (gamepad1.left_stick_y));
+                rightMotorBack.setPower(multiplier * (gamepad1.left_stick_y));
             }
             else {
                 leftMotorFront.setPower(0);
@@ -134,27 +128,36 @@ public class TeleOpAlpha extends LinearOpMode {
                 rightMotorBack.setPower(0);
             }
             //Claw closing and arm upping
-            if (gamepad1.right_bumper && !gamepad1.left_bumper && armCooldown.seconds() > 2) {
+            //negative brings arm towards picking up
+            //positive brings claw servo towards picking up
+            if (gamepad1.right_bumper && !gamepad1.left_bumper && armCooldown.seconds() < 6) {
                 armCooldown.reset();
-                armServo.setPosition(90);
-                sleep(500);
-                clawServo.setPosition(90);
-                sleep(500);
-                armServo.setPosition(0);
+                armServo.setPower(-1);
+                sleep(1000);
+                clawServo.setPower(1);
+                armServo.setPower(0);
+                sleep(4000);
+                armServo.setPower(1);
+                clawServo.setPower(0);
+                sleep(1000);
+                armServo.setPower(0);
             }
             //Claw opening and arm downing
-            else if (!gamepad1.right_bumper && gamepad1.left_bumper && armCooldown.seconds() > 2) {
+            else if (!gamepad1.right_bumper && gamepad1.left_bumper && armCooldown.seconds() < 3) {
                 armCooldown.reset();
-                armServo.setPosition(90);
-                sleep(500);
-                clawServo.setPosition(0);
-                sleep(500);
-                armServo.setPosition(0);
+                clawServo.setPower(0.8);
+                sleep(1000);
+                armServo.setPower(0.8);
+                clawServo.setPower(0);
+                sleep(1000);
+                armServo.setPower(0);
             }
         }
         leftMotorFront.setPower(0);
         leftMotorBack.setPower(0);
         rightMotorFront.setPower(0);
         rightMotorBack.setPower(0);
+        clawServo.setPower(0);
+        armServo.setPower(0);
     }
 }
